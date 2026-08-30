e4484a92c351cfda8ecc69a2439f1fc11e374e7e
Alexander <kq1time.me@mail.ru>
2026-08-30 16:15:52 +0300

    Create log.py

---
+++ log.py
@@ -0,0 +1,22 @@
+# Logging
+# version 0.0.1
+
+from datetime import datetime
+
+def create_log(log_type: str, event: str) -> str:
+    now = datetime.now()
+    new_time = now.replace(microsecond=0)
+
+    return f"{log_type}: [{new_time}] {event}"
+
+def write_log(log: str):
+    with open("events.log", "a") as f:
+        f.write(log)
+        f.write("\n")
+
+# Examples 
+new_log = create_log("INFO", "Program started")
+write_log(new_log)
+
+log_2 = create_log("INFO", "Program finished")
+write_log(log_2)
